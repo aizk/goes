68c4913be44916ba44c66ae6bf0c5df8b8ae1950
system <78435431@qq.com>
2017-11-30 17:31:27 +0800

    添加分类查询接口

--- controller/category/category.go
+++ controller/category/category.go
@@ -7,6 +7,7 @@ import (
 	"github.com/goes/controller/common"
 	"strings"
 	"strconv"
+	"unicode/utf8"
 )
 
 func Save(ctx *iris.Context, edit bool) {
@@ -31,6 +32,17 @@ func Save(ctx *iris.Context, edit bool) {
 	if category.Sequence < minOrder || category.Sequence > maxOrder {
 		msg := "分类的排序要在" + strconv.Itoa(minOrder) + "到" + strconv.Itoa(maxOrder) + "之间"
 		common.SendErrorJSON(msg, ctx)
+		return
+	}
+
+	if utf8.RuneCountInString(category.Name) > config.ServerConfig.MaxNameLength {
+		msg := "分类名称不能超过" + strconv.Itoa(config.ServerConfig.MaxNameLength) + "个字符"
+		common.SendErrorJSON(msg, ctx)
+		return
+	}
+
+	if category.Status != model.CategoryStatusOpen && category.Status != model.CategoryStatusClose {
+		common.SendErrorJSON("无效的 status，必须为1 或者 2", ctx)
 		return
 	}
 
@@ -95,3 +107,60 @@ func Update(ctx *iris.Context) {
 	Save(ctx, true)
 }
 
+// 获取分类信息
+func Info(ctx *iris.Context) {
+	id, err := ctx.ParamInt("id")
+	if err != nil {
+		common.SendErrorJSON("错误的 ID 类型", ctx)
+		return
+	}
+
+	var category model.Category
+	if err := model.DB.First(&category, id).Error; err != nil {
+	    common.SendErrorJSON("查询ID失败", ctx)
+	    return
+	}
+
+	ctx.JSON(iris.StatusOK, iris.Map{
+		"errCode": model.SUCCESS,
+		"message": "success",
+		"data": iris.Map{
+			"category": category,
+		},
+	})
+}
+
+// 更新分类状态 id status
+
+// 获取 status = 1 的可用分类列表
+
+// 获取所有分类
+func FetchAllCategory(ctx *iris.Context) {
+	var categories []model.Category
+	pageNum, err := strconv.Atoi(ctx.FormValue("pageNum"))
+	if err != nil || pageNum < 1 {
+		pageNum = 1
+	}
+
+	// 默认为按照创建时间降序排列
+	orderString := "created_at"
+	if ctx.FormValue("asc") == "1" {
+		orderString += " asc"
+	} else {
+		orderString += " desc"
+	}
+
+	offset := (pageNum - 1) * config.ServerConfig.PageSize
+	if err := model.DB.Offset(offset).Limit(config.ServerConfig.PageSize).Order(orderString).Find(&categories).Error; err != nil {
+		common.SendErrorJSON("查询失败", ctx)
+		return
+	}
+
+	ctx.JSON(iris.StatusOK, iris.Map{
+		"errCode" : model.SUCCESS,
+		"message"   : "success",
+		"data"  : iris.Map{
+			"categories": categories,
+		},
+	})
+}
